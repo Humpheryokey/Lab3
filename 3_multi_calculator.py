83f5db4f3f1cc360d28b72b3ec8ac376bdf24781
Humpheryokey <g12460704@thu.edu.tw>
2024-03-18 13:20:57 +0800

    Humphery

---
+++ 3_multi_calculator.py
@@ -0,0 +1,20 @@
+# Humphery
+# Calculator to add two numbers and display the result
+
+# 1. Input
+x1 = input ("Type x1: ")
+x2 = input ("Type x2: ")
+op = input (" Type operator:")
+
+# 2. Process
+if op == "+":
+    sum = int (x1) + int (x2)
+elif op == "-":
+    sum = int (x1) - int (x2)
+elif op == "*":
+    sum = int (x1) * int (x2)
+
+
+#3. Output
+
+print (f"Sum: {sum}")
