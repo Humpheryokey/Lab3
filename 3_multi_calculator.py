e7b7b50726c0920cd744d1de5880c9f37d7261dd
Humpheryokey <g12460704@thu.edu.tw>
2024-03-25 12:45:40 +0800

    Humphery

--- 3_multi_calculator.py
+++ 3_multi_calculator.py
@@ -1,20 +1,28 @@
-# Humphery
-# Calculator to add two numbers and display the result
+    # Humphery
+    # Calculator to add two numbers and display the result
+print(f"calculator")
 
+sum = 0
 
-# 1. Input
-x1 = input ("Type x1:")
-x2 = input ("Type x2:")
-op = input ("Type operator:")
+while True:
+    # 1. Input
+    x1 = input ("Enter first number : ")
+    x2 = input ("Enter first number : ")
+    op = input ("Enter operator     : ")
 
-# 2. Process
-if op == "+":
-    sum = int (x1) + int (x2)
-elif op == "-":
-    sum = int (x1) - int (x2)
-elif op == "*":
-    sum = int (x1) * int (x2)
-
-
-#3. Output
-print (f"Sum: {sum}")
+    # 2. Process
+    if op == "+":
+        sum = int (x1) + int (x2)
+    elif op == "-":
+        sum = int (x1) - int (x2)
+    elif op == "*":
+        sum = int (x1) * int (x2)
+    elif op == "/":
+        sum = int (x1) / int (x2)
+    
+    #3. Output
+    print (f"Sum                : {sum}")
+    res = input("continue? (Yes/No) : ")
+    if res == "No":
+        print(f"Exit")
+        break;
